e542a387b43ffa191f5db428d3b64de477855a8e
griphaugland <griphaugland@gmail.com>
2023-09-29 21:50:05 +0200

    Initial commit, rendered project start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <Nav/>
+        <Hero/>
+    </>
+  )
+}
+
+export default App
